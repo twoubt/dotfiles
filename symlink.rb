2852feed31b9fd563762ec0c7a88a9e66e19cdff
Aftermaths <fnzzhou@gmail.com>
2015-10-18 02:21:21 +0100

    updated symlink for powerline

--- symlink.rb
+++ symlink.rb
@@ -1,14 +1,22 @@
 files = Dir.glob("*")
-ignored = ['README.md', 'com.googlecode.iterm2.plist', 'term_colours', 'symlink.rb', 'Brewfile.sh', 'cleanup.sh']
+ignored = ['powerline', 'README.md', 'com.googlecode.iterm2.plist', 'term_colours', 'symlink.rb', 'Brewfile.sh', 'cleanup.sh']
 dotfiles = []
+
+def file_path(file)
+  File.expand_path(file, File.dirname(__FILE__))
+end
+
+def home_path(file, path = '.')
+  "/Users/Zhou/" + path + file
+end
 
 files.each do |file|
 	ignored.include?(file) ? '' : dotfiles << file
 end
 
 dotfiles.each do |file|
-  home_path = File.join('/Users', 'Zhou', '.' + file)
-  file_path = File.expand_path(file, File.dirname(__FILE__))
-  File.delete(home_path) if File.exist?(home_path)
-  File.symlink(file_path, home_path)
+  File.delete(home_path(file)) if File.exist?(home_path(file))
+  File.symlink(file_path(file), home_path(file))
 end
+
+File.symlink(file_path(ignored[0]), home_path(ignored[0], '.config/'))

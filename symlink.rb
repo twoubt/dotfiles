7175ddc21dcc466cf6d9c28ab9f29b35160bff9b
Aftermaths <fnzzhou@gmail.com>
2015-10-19 04:24:40 +0100

    removed powerline

--- symlink.rb
+++ symlink.rb
@@ -3,7 +3,6 @@ require 'fileutils'
 files = Dir.glob("*")
 @ignore_files = ['README.md', 'com.googlecode.iterm2.plist', 'term_colours', 'symlink.rb', 'Brewfile.sh', 'cleanup.sh']
 @bin = ['tat']
-@powerline = ['powerline']
 @dotfiles = []
 
 def source_from(file)
@@ -22,7 +21,7 @@ def move_existing(file, path)
 end
 
 def skip_file?(file)
-  @ignore_files.include?(file) || @bin.include?(file) || @powerline.include?(file)
+  @ignore_files.include?(file) || @bin.include?(file)
 end
 
 files.each do |file|
@@ -38,9 +37,3 @@ end
   move_existing(file, '/usr/local/bin/')
   File.symlink(source_from(file), source_to(file, '/usr/local/bin/'))
 end
-
-@powerline.each do |file|
-  move_existing(file, '~/.config/')
-  File.symlink(source_from(file), source_to(file, '~/.config'))
-end
-
